64db8ab66880ae2f40c5bd58014b2f5b4c787c5b
Michael Sledge <michael.sledge@incrowdsports.com>
2017-07-26 15:37:01 +0100

    convert sed command to powershell

--- bin/smp_project_generate_lgpl_auto.bat
+++ bin/smp_project_generate_lgpl_auto.bat
@@ -81,7 +81,7 @@ REM Run the executable
 ECHO Running project generator...
 project_generate.exe %PGOPTIONS%
 REM fix missing config.asm inclusion
-C:\msys64\usr\bin\sed -i'' "/PreIncludeFiles.*config\.asm/a       <AdditionalOptions>-P\"config.asm\"</AdditionalOptions>" ../../ffmpeg/smp/*.vcxproj
+powershell -nologo -noprofile -command "(Get-Item .\*.vcxproj) | ForEach-Object -Process {(Get-Content $_) | Foreach-Object {$_ -replace \"^<YASM^>\", \"^<YASM^>`n ` ` ` ` ` ^<AdditionalOptions^>-P`\"config.asm`\"^</AdditionalOptions^>\" } | Set-Content $_ }"
 GOTO exit
 
 :makeGetDeps
